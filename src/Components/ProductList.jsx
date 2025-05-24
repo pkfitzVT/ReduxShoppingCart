8e6256eeaa93568254bff51b49c38f43bda433cb
pkfitzVT <pkeenanfitzgerald@gmail.com>
2025-05-24 10:11:37 -0400

    added product list details to ProductList.jsx

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+
 import './ProductList.css'; 
 
 const ProductList = () => {
@@ -13,7 +13,14 @@ const ProductList = () => {
     <div className="product-list">
       <h2 className="product-list-title">Products</h2>
       <ul className="product-list-items">
-     
+          {products.map(product => (
+              <li key={product.id} className="product-list-item">
+                  <span>{product.name} - ${product.price}</span>
+                  <button>
+                      Add to Cart
+                  </button>
+              </li>
+          ))}
       </ul>
     </div>
   );
